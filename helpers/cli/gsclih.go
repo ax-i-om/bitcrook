ccc2885acc0f669812def6f1e3ddadd455c8af9a
audio <57118993+audioo@users.noreply.github.com>
2021-02-17 13:40:08 -0500

    Add files via upload

---
+++ helpers/cli/gsclih.go
@@ -0,0 +1,55 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+// Clear ...
+func Clear() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+// Banner ...
+func Banner() {
+	Clear()
+	fmt.Println("")
+
+	fmt.Println("     ▄██████▄   ▄██████▄     ▄████████    ▄████████    ▄████████    ▄█   ▄█▄ ")
+	fmt.Println("    ███    ███ ███    ███   ███    ███   ███    ███   ███    ███   ███ ▄███▀ ")
+	fmt.Println("    ███    █▀  ███    ███   ███    █▀    ███    █▀    ███    █▀    ███▐██▀   ")
+	fmt.Println("   ▄███        ███    ███   ███         ▄███▄▄▄      ▄███▄▄▄      ▄█████▀    ")
+	fmt.Println("  ▀▀███ ████▄  ███    ███ ▀███████████ ▀▀███▀▀▀     ▀▀███▀▀▀     ▀▀█████▄    ")
+	fmt.Println("    ███    ███ ███    ███          ███   ███    █▄    ███    █▄    ███▐██▄   ")
+	fmt.Println("    ███    ███ ███    ███    ▄█    ███   ███    ███   ███    ███   ███ ▀███▄ ")
+	fmt.Println("    ████████▀   ▀██████▀   ▄████████▀    ██████████   ██████████   ███   ▀█▀ ")
+	fmt.Println("                                                                   ▀         ")
+
+	fmt.Println("")
+
+}
+
+// Dispban ...
+func Dispban(text string) {
+	var reset string = "\033[0m"
+	var red string = "\033[31m"
+	fmt.Println("   [ " + string(red) + text + string(reset) + " ]")
+}
+
+// Dispop ...
+func Dispop(key string, value string) {
+	var reset string = "\033[0m"
+	var red string = "\033[31m"
+	fmt.Println("   [ " + string(red) + key + string(reset) + " ] - " + value)
+}
+
+// Dispopg ...
+func Dispopg(key string, value string) {
+	var reset string = "\033[0m"
+	var green string = "\033[32m"
+	fmt.Println("   [ " + string(green) + key + string(reset) + " ] - " + value)
+
+}
