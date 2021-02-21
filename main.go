3ab5fcb277eb544c72e992d595dc0e554ef1b8e1
audio <57118993+audioo@users.noreply.github.com>
2021-02-21 13:20:38 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -1,42 +1,80 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"sync"
 
 	"github.com/audioo/goseek/helpers/cli"
-	"github.com/audioo/goseek/helpers/ent"
-	"github.com/audioo/goseek/helpers/http"
-	"github.com/audioo/goseek/helpers/load"
+	"github.com/audioo/goseek/helpers/run"
 )
 
 func main() {
 
 	var wg sync.WaitGroup
 	var userres string
+	var writeIt bool
+	flag.BoolVar(&writeIt, "w", false, "Write results to a text file.")
+	flag.StringVar(&userres, "u", "abc123anonymous!!bbzzn234whatttt", "Username input.")
 
-	cli.Banner()
-	cli.Dispopg("GREEN", "ACCOUNT EXISTS")
-	cli.Dispop("RED", "ACCOUNT DOES NOT EXIST")
-	fmt.Println("")
-	fmt.Println("   Enter Username")
-	fmt.Print("   >> ")
-	fmt.Scan(&userres)
-	fmt.Println("")
+	flag.Parse()
+	if writeIt {
 
-	var arrNo []ent.Website = load.NoRedirSites(userres)
-	for _, v := range arrNo {
-		http.GetSCnoredir(v.Title, v.Domain, &wg)
+		if userres == "abc123anonymous!!bbzzn234whatttt" {
+			fmt.Println("")
+			fmt.Println("Command format:")
+			fmt.Println("go run main.go -u=user -w=write")
+			fmt.Println("")
+			fmt.Println("Example:")
+			fmt.Println("go run main.go =u=audioo -w=false")
+			fmt.Println("")
+		} else {
+			cli.Banner()
+			fmt.Println("True")
+			cli.Dispopg("GREEN", "ACCOUNT EXISTS")
+			cli.Dispop("RED", "ACCOUNT DOES NOT EXIST")
+			fmt.Println("")
+			run.SendSeeker(userres, &wg)
+			wg.Wait()
+			fmt.Println("")
+			cli.Dispban("Go-Seek Process Complete")
+		}
+	} else {
+		if userres == "abc123anonymous!!bbzzn234whatttt" {
+			fmt.Println("")
+			fmt.Println("Command format:")
+			fmt.Println("go run main.go -u=user -w=write")
+			fmt.Println("")
+			fmt.Println("Example:")
+			fmt.Println("go run main.go =u=audioo -w=false")
+			fmt.Println("")
+		} else {
+			cli.Banner()
+			fmt.Println("False")
+			cli.Dispopg("GREEN", "ACCOUNT EXISTS")
+			cli.Dispop("RED", "ACCOUNT DOES NOT EXIST")
+			fmt.Println("")
+			run.SendSeeker(userres, &wg)
+			wg.Wait()
+			fmt.Println("")
+			cli.Dispban("Go-Seek Process Complete")
+		}
 	}
-	var arrYes []ent.Website = load.RedirSites(userres)
-	for _, v := range arrYes {
-		http.GetSCredir(v.Title, v.Domain, &wg)
-	}
-
-	wg.Wait()
-	fmt.Println("")
-	cli.Dispban("Go-Seek Process Complete")
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Scan(&userres)
+	/*
+		cli.Banner()
+		cli.Dispopg("GREEN", "ACCOUNT EXISTS")
+		cli.Dispop("RED", "ACCOUNT DOES NOT EXIST")
+		fmt.Println("")
+		fmt.Println("   Enter Username")
+		fmt.Print("   >> ")
+		fmt.Scan(&userres)
+		fmt.Println("")
+		run.SendSeeker(userres, &wg)
+		wg.Wait()
+		fmt.Println("")
+		cli.Dispban("Go-Seek Process Complete")
+		fmt.Println("")
+		fmt.Println("")
+		fmt.Scan(&userres)
+	*/
 }
