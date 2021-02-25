4c4c5066a9bf7049d454ce2e65a3e51699da230f
audio <57118993+audioo@users.noreply.github.com>
2021-02-25 15:13:24 -0500

    Add files via upload

--- helpers/http/gshttph.go
+++ helpers/http/gshttph.go
@@ -12,47 +12,20 @@ import (
 )
 
 // GetSCredir ... Automatically follows HTTP redirects.
-func GetSCredir(title string, url string, wg *sync.WaitGroup) {
-	wg.Add(1)
-	defer wg.Done()
-	method := "GET"
-
-	client := &http.Client{}
-	req, err := http.NewRequest(method, url, nil)
-
-	if err != nil {
-		cli.Dispop(title, url)
-		return
-	}
-	res, err := client.Do(req)
-	if err != nil {
-		cli.Dispop(title, url)
-		return
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		cli.Dispop(title, url)
-		return
-	}
-	strconv.AppendBool(body, true)
-	if res.StatusCode == 200 {
-		cli.Dispopg(title, url)
-	} else {
-		cli.Dispop(title, url)
-	}
-}
-
-// GetSCredirWrite ... Automatically follows HTTP redirects.
-func GetSCredirWrite(title string, url string, wg *sync.WaitGroup, userres string) {
+func GetSCredir(title string, url string, wg *sync.WaitGroup, userres string, write bool) {
 	file, err := os.OpenFile(userres+".txt", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0660)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	if !write {
+		file.Close()
+		os.Remove(userres + ".txt")
+	} else {
+		defer file.Close()
+	}
 	wg.Add(1)
-	defer file.Close()
+
 	defer wg.Done()
 	method := "GET"
 
@@ -78,59 +51,25 @@ func GetSCredirWrite(title string, url string, wg *sync.WaitGroup, userres strin
 	strconv.AppendBool(body, true)
 	if res.StatusCode == 200 {
 		cli.Dispopg(title, url)
-		fmt.Fprintf(file, "%s\n", title+" => "+url)
+		if write {
+			fmt.Fprintf(file, "%s\n", title+" => "+url)
+		}
+
 	} else {
 		cli.Dispop(title, url)
 	}
 }
 
 // GetSCnoredir ... Does not automatically follow HTTP redirects.
-func GetSCnoredir(title string, url string, wg *sync.WaitGroup) {
-
-	wg.Add(1)
-	defer wg.Done()
-	method := "GET"
-
-	client := &http.Client{
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		},
-	}
-	req, err := http.NewRequest(method, url, nil)
-
-	if err != nil {
-		cli.Dispop(title, url)
-		return
-	}
-	res, err := client.Do(req)
-	if err != nil {
-		cli.Dispop(title, url)
-		return
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		cli.Dispop(title, url)
-		return
-	}
-	strconv.AppendBool(body, true)
-	if res.StatusCode == 200 {
-		cli.Dispopg(title, url)
-	} else {
-		cli.Dispop(title, url)
-	}
-}
-
-// GetSCnoredirWrite ... Does not automatically follow HTTP redirects.
-func GetSCnoredirWrite(title string, url string, wg *sync.WaitGroup, userres string) {
+func GetSCnoredir(title string, url string, wg *sync.WaitGroup, userres string, write bool) {
 	file, err := os.OpenFile(userres+".txt", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0660)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	wg.Add(1)
 	defer file.Close()
+	wg.Add(1)
+
 	defer wg.Done()
 	method := "GET"
 
@@ -160,7 +99,9 @@ func GetSCnoredirWrite(title string, url string, wg *sync.WaitGroup, userres str
 	strconv.AppendBool(body, true)
 	if res.StatusCode == 200 {
 		cli.Dispopg(title, url)
-		fmt.Fprintf(file, "%s\n", title+" => "+url)
+		if write {
+			fmt.Fprintf(file, "%s\n", title+" => "+url)
+		}
 	} else {
 		cli.Dispop(title, url)
 	}
