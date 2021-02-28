4a9e912cc5758d777f1ed74288f5af6a8bb5dd47
audio <57118993+audioo@users.noreply.github.com>
2021-02-28 12:20:00 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -3,59 +3,54 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"sync"
 
 	"github.com/audioo/goseek/helpers/cli"
 	"github.com/audioo/goseek/helpers/run"
 )
 
+var (
+	userres string
+	write   bool
+)
+
+func init() {
+
+	flag.BoolVar(&write, "w", false, "Write to File")
+	flag.Parse()
+	if !(len(flag.Args()) > 0) {
+		fmt.Println("\nProper Command Format: ")
+		fmt.Println("go run main.go [flags] username")
+		fmt.Println("")
+		flag.Usage()
+		os.Exit(1)
+	}
+	userres = flag.Args()[0]
+
+}
+
 func main() {
 
 	var wg sync.WaitGroup
-	var userres string
-	var writeIt bool
-	flag.BoolVar(&writeIt, "w", false, "Write results to a text file.")
-	flag.StringVar(&userres, "u", "abc123anonymous!!bbzzn234whatttt", "Username input.")
 
-	flag.Parse()
-	if writeIt {
-
-		if userres == "abc123anonymous!!bbzzn234whatttt" {
-			fmt.Println("")
-			fmt.Println("Command format:")
-			fmt.Println("go run main.go -u=user -w=write")
-			fmt.Println("")
-			fmt.Println("Example:")
-			fmt.Println("go run main.go -u=audioo -w=false")
-			fmt.Println("")
-		} else {
-			cli.Banner()
-			cli.Dispopg("GREEN", "ACCOUNT EXISTS")
-			cli.Dispop("RED", "ACCOUNT DOES NOT EXIST")
-			fmt.Println("")
-			run.SendSeeker(userres, &wg, true)
-			wg.Wait()
-			fmt.Println("")
-			cli.Dispban("Go-Seek Process Complete")
-		}
+	if write {
+		cli.Banner()
+		cli.Dispopg("GREEN", "ACCOUNT EXISTS")
+		cli.Dispop("RED", "ACCOUNT DOES NOT EXIST")
+		fmt.Println("")
+		run.SendSeeker(userres, &wg, true)
+		wg.Wait()
+		fmt.Println("")
+		cli.Dispban("Go-Seek Process Complete :: Results written to " + userres + ".txt")
 	} else {
-		if userres == "abc123anonymous!!bbzzn234whatttt" {
-			fmt.Println("")
-			fmt.Println("Command format:")
-			fmt.Println("go run main.go -u=user -w=write")
-			fmt.Println("")
-			fmt.Println("Example:")
-			fmt.Println("go run main.go -u=audioo -w=false")
-			fmt.Println("")
-		} else {
-			cli.Banner()
-			cli.Dispopg("GREEN", "ACCOUNT EXISTS")
-			cli.Dispop("RED", "ACCOUNT DOES NOT EXIST")
-			fmt.Println("")
-			run.SendSeeker(userres, &wg, false)
-			wg.Wait()
-			fmt.Println("")
-			cli.Dispban("Go-Seek Process Complete")
-		}
+		cli.Banner()
+		cli.Dispopg("GREEN", "ACCOUNT EXISTS")
+		cli.Dispop("RED", "ACCOUNT DOES NOT EXIST")
+		fmt.Println("")
+		run.SendSeeker(userres, &wg, false)
+		wg.Wait()
+		fmt.Println("")
+		cli.Dispban("Go-Seek Process Complete :: Results have not been written")
 	}
 }
