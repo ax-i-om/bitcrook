8947abf9d8ddb543feb9b9e794db0c51f5f19e96
audio <57118993+audioo@users.noreply.github.com>
2021-03-22 14:04:00 -0400

    Add files via upload

--- helpers/http/gshttph.go
+++ helpers/http/gshttph.go
@@ -6,13 +6,12 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"sync"
 
 	"github.com/audioo/goseek/helpers/ent"
 )
 
-// GetSCredir ... Automatically follows HTTP redirects.
-func GetSCredir(title string, url string, wg *sync.WaitGroup, userres string, write bool) ent.WebsiteRes {
+// GetSCredir ...
+func GetSCredir(title string, url string, userres string, write bool, redirect bool) ent.WebsiteRes {
 	file, err := os.OpenFile(userres+".txt", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0660)
 	if err != nil {
 		// fmt.Println(err)
@@ -23,12 +22,18 @@ func GetSCredir(title string, url string, wg *sync.WaitGroup, userres string, wr
 	} else {
 		defer file.Close()
 	}
-	wg.Add(1)
 
-	defer wg.Done()
 	method := "GET"
-
-	client := &http.Client{}
+	var client *http.Client
+	if redirect {
+		client = &http.Client{}
+	} else {
+		client = &http.Client{
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				return http.ErrUseLastResponse
+			},
+		}
+	}
 	req, err := http.NewRequest(method, url, nil)
 
 	if err != nil {
@@ -53,47 +58,4 @@ func GetSCredir(title string, url string, wg *sync.WaitGroup, userres string, wr
 	}
 	return ent.WebsiteRes{Title: title, Domain: url, Valid: false}
 
-}
-
-// GetSCnoredir ... Does not automatically follow HTTP redirects.
-func GetSCnoredir(title string, url string, wg *sync.WaitGroup, userres string, write bool) ent.WebsiteRes {
-	file, err := os.OpenFile(userres+".txt", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0660)
-	if err != nil {
-		// fmt.Println(err)
-		// return
-	}
-	defer file.Close()
-	wg.Add(1)
-
-	defer wg.Done()
-	method := "GET"
-
-	client := &http.Client{
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		},
-	}
-	req, err := http.NewRequest(method, url, nil)
-
-	if err != nil {
-		return ent.WebsiteRes{Title: title, Domain: url, Valid: false}
-	}
-	res, err := client.Do(req)
-	if err != nil {
-		return ent.WebsiteRes{Title: title, Domain: url, Valid: false}
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return ent.WebsiteRes{Title: title, Domain: url, Valid: false}
-	}
-	strconv.AppendBool(body, true)
-	if res.StatusCode == 200 {
-		if write {
-			fmt.Fprintf(file, "%s\n", title+" => "+url)
-		}
-		return ent.WebsiteRes{Title: title, Domain: url, Valid: true}
-	}
-	return ent.WebsiteRes{Title: title, Domain: url, Valid: false}
 }
