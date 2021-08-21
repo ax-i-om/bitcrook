dd74f0a8261947909e816821200542d582e19d6c
Hyper Audio <maraudery@protonmail.com>
2021-08-21 00:50:47 -0400

    Update IP Lookup page.

--- api/public/js/app.js
+++ api/public/js/app.js
@@ -1,7 +1,27 @@
 $('#ipsearch').on('click', () => {
     ip = $('#ip').val()
     $.getJSON('http://localhost:6174/ip/' + ip, (res) => {
-        $('#ipresult').val(JSON.stringify(res, null, 4))
+        var obj = JSON.parse(res);
+        $('#ipresult').val("Status: " + obj.status + "\n" +
+        "Status: " + obj.status + "\n" +
+        "Continent: " + obj.continent + "\n" +
+        "Country: " + obj.country + "\n" +
+        "Region Name: " + obj.regionname + "\n" +
+        "City: " + obj.district + "\n" +
+        "District: " + obj.zip + "\n" +
+        "Zip: " + obj.lat + "\n" +
+        "Latitude: " + obj.lon + "\n" +
+        "Longitude: " + obj.timezone + "\n" +
+        "Time Zone: " + obj.currency + "\n" +
+        "Currency: " + obj.isp + "\n" +
+        "ISP: " + obj.org + "\n" +
+        "Org: " + obj.as + "\n" +
+        "As: " + obj.asname + "\n" +
+        "Asname: " + obj.reverse + "\n" +
+        "Reverse: " + obj.mobile + "\n" +
+        "Mobile: " + obj.proxy + "\n" +
+        "Proxy: " + obj.hosting + "\n" +
+        "Hosting: " + obj.query + "\n")
     })
     $('#ipresult').val('')
 })
