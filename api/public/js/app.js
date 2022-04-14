59ee6c1fa3b5e524adc4be6de5608ba8311874d0
audioo <audioo@ctemplar.com>
2022-04-14 14:10:29 -0400

    fix VIN lookup API/GUI

--- api/public/js/app.js
+++ api/public/js/app.js
@@ -36,41 +36,27 @@ $('#vinsearch').on('click', () => {
     vin = $('#vin').val()
     $.getJSON('http://localhost:6174/vin/' + vin, (res) => {
         $('#vinresult').val("Vin: " + res.Vin + "\n" +
-        "Year: " + res.Year + "\n" +
         "Make: " + res.Make + "\n" +
         "Model: " + res.Model + "\n" +
+        "Year: " + res.Year + "\n" +
         "Trim: " + res.Trim + "\n" +
-        "Short Trim: " + res.ShortTrim + "\n" +
-        "Trim Variations: " + res.TrimVariations + "\n" +
-        "Made In: " + res.MadeIn + "\n" +
-        "Made In City: " + res.MadeInCity + "\n" +
-        "Vehicle Style: " + res.VehicleStyle + "\n" +
-        "Vehicle Type: " + res.VehicleType + "\n" +
-        "Vehicle Size: " + res.VehicleSize + "\n" +
-        "Vehicle Category: " + res.VehicleCategory + "\n" +
-        "Doors: " + res.Doors + "\n" +
-        "Fuel Type: " + res.FuelType + "\n" +
-        "Fuel Capacity: " + res.FuelCapacity + "\n" +
-        "City Mileage: " + res.CityMileage + "\n" +
-        "Highway Mileage: " + res.HighwayMileage + "\n" +
+        "Body: " + res.Body + "\n" +
         "Engine: " + res.Engine + "\n" +
-        "Engine Size: " + res.EngineSize + "\n" +
-        "Engine Cylinders: " + res.EngineCylinders + "\n" +
-        "Transmission Type: " + res.TransmissionType + "\n" +
-        "Transmission Gears: " + res.TransmissionGears + "\n" +
-        "Driven Wheels: " + res.DrivenWheels + "\n" +
-        "Anti-Brake System: " + res.AntiBrakeSystem + "\n" +
+        "Manufactured In: " + res.ManufacturedIn + "\n" +
+        "Trim Level: " + res.TrimLevel + "\n" +
         "Steering Type: " + res.SteeringType + "\n" +
-        "Curb Weight: " + res.CurbWeight + "\n" +
-        "Gross Weight: " + res.GrossWeight + "\n" +
+        "ABS: " + res.Abs + "\n" +
+        "Tank Size: " + res.TankSize + "\n" +
         "Overall Height: " + res.OverallHeight + "\n" +
         "Overall Length: " + res.OverallLength + "\n" +
         "Overall Width: " + res.OverallWidth + "\n" +
         "Standard Seating: " + res.StandardSeating + "\n" +
         "Optional Seating: " + res.OptionalSeating + "\n" +
-        "Invoice Price: " + res.InvoicePrice + "\n" +
-        "Delivery Charges: " + res.DeliveryCharges + "\n" +
-        "MSRP: " + res.MSRP)
+        "Highway Mileage: " + res.HighwayMileage + "\n" +
+        "City Mileage: " + res.CityMileage + "\n" +
+        "Fuel Type: " + res.FuelType + "\n" +
+        "Drive Type: " + res.DriveType + "\n" +
+        "Transmission: " + res.Transmission)
     })
     $('#vinresult').val('') 
 })
